7b7dbd58e0ebb379c71aa211096fd9e0f6c37817
Leo Dalbert <leodalbert@Leos-MBP-2.home>
2020-04-20 17:42:20 -0400

    setup test databse environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/database'
 import expenses from '../tests/fixtures/expenses'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDqViKp-sAa-x3zT_pQbpbaX_TUYo6dFWQ",
-    authDomain: "expensify-196c2.firebaseapp.com",
-    databaseURL: "https://expensify-196c2.firebaseio.com",
-    projectId: "expensify-196c2",
-    storageBucket: "expensify-196c2.appspot.com",
-    messagingSenderId: "689380195420",
-    appId: "1:689380195420:web:ba3da69610ca498d63fa99"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
