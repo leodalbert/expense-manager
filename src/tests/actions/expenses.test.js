b5337a80f725d689ef0a1edbc77d5f06f5099690
Leo Dalbert <leodalbert@Leos-MBP-2.home>
2020-04-22 14:28:44 -0400

    Wired up Edit Expense and Remove Expense to firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense, 
   setExpenses, 
   startSetExpenses, 
-  startRemoveExpense 
+  startRemoveExpense, 
+  startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -56,6 +57,29 @@ test('should setup edit expense action object', () => {
     }
   });
 });
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({})
+  const id = expenses[2].id
+  const updates = {
+    description: 'MasterCard',
+    amount: 101000,
+    note: 'test',
+    createdAt: 50000000
+  }
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    })
+    return database.ref(`expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toEqual(updates)
+    done()
+  })
+})
 
 test('should setup add expense action object with provided values', () => {
   const action = addExpense(expenses[2]);
